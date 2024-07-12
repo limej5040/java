5128024044646e96c2e187614ff4de6c4dea847b
eunjung <eunjung@localhost>
2024-07-13 01:25:12 +0900

    java study

---
+++ JavaStudy/src/chap_01/_03_Variables.java
@@ -0,0 +1,35 @@
+package chap_01;
+
+public class _03_Variables {
+	public static void main(String[] args) {
+		String name = "나도 코딩";
+		// name = "나도 코딩";
+		int hour = 15;
+
+		System.out.println(name + "님, 배송이 시작됩니다. " + hour + "시에 방문 예정입니다 ");
+		System.out.println(name + "님, 배송이완료되었습니다 ");
+
+		double score = 90.5;
+		char grade = 'A';
+		name = "강백호";
+
+		System.out.println(name + "님의 평균 점수는 " + score + "점입니다.");
+		System.out.println("학점은 " + grade + "입니다.");
+
+		boolean pass = true;
+		System.out.println("이번 시험에 합격 했을까요?" + pass);
+
+		double d = 3.14123456789;
+		float f = 3.14123456789F; // 정밀한 소수점을 출력할 수 없다.
+		// 실수를 float 자료형으로 만들기 위해서 뒤에 f or F를 적어줘야한다.
+
+		System.out.println(d);
+		System.out.println(f);
+
+		long l = 1000000000000L;
+		l = 1_000_000_000_000L;
+
+		System.out.println(l);
+
+	}
+}
