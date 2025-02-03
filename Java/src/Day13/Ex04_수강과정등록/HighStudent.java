c1e0a6db90246faff3cee2ceeac3431873ea42f0
eunjung <eunjung@localhost>
2025-02-03 20:42:06 +0900

    java study

---
+++ Java/src/Day13/Ex04_수강과정등록/HighStudent.java
@@ -0,0 +1,17 @@
+package Day13.Ex04_수강과정등록;
+
+public class HighStudent extends Student {
+
+	public HighStudent(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "HightStudent [" + this.getName() + "]";
+	}
+	
+
+	
+}
